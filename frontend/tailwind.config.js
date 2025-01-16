a2822a7b6393af57ce5243fd458063b9c5e26606
Shubh Nawani <shubhnawani@outlook.com>
2025-01-16 10:47:49 +0530

    Introduced Login Page

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Path to your source files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
